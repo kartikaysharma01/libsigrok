3cc16a25ac369ce57263bc37bb31cb43fe05eb08
Kartikay Sharma <sharma.kartik2107@gmail.com>
2021-06-10 20:50:57 +0000

    impl option based scan

--- src/hardware/pslab/api.c
+++ src/hardware/pslab/api.c
@@ -56,8 +56,8 @@ static struct sr_dev_driver pslab_driver_info;
 
 static GSList *scan(struct sr_dev_driver *di, GSList *options)
 {
-	(void) options;
 	GSList *l, *devices;
+	struct sr_config *src;
 
 	GSList *device_paths_v5 = sr_serial_find_usb(0x04D8, 0x00DF);
 
@@ -69,9 +69,24 @@ static GSList *scan(struct sr_dev_driver *di, GSList *options)
 	struct sr_serial_dev_inst *serial;
 	struct sr_dev_inst *sdi;
 
+	const char *path;
+	const char *serialcomm = "1000000/8n1";
+
+	for (l = options; l; l = l->next) {
+		src = l->data;
+		switch (src->key) {
+			case SR_CONF_CONN:
+				path = g_variant_get_string(src->data, NULL);
+				device_paths = g_slist_append(NULL, path);
+				break;
+			case SR_CONF_SERIALCOMM:
+				serialcomm = g_variant_get_string(src->data, NULL);
+				break;
+		}
+	}
 	for (l = device_paths; l; l = l->next)
 	{
-		serial = sr_serial_dev_inst_new(l->data, "1000000/8n1");
+		serial = sr_serial_dev_inst_new(l->data, serialcomm);
 
 		sdi = g_new0(struct sr_dev_inst, 1);
 		sdi->status = SR_ST_INACTIVE;
