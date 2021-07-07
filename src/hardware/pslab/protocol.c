7a4220002500016d36cfe20b6786d721d4709f68
Bhaskar Sharma <bhaskar.sharma@groww.in>
2021-07-07 06:41:22 +0530

    working perfectly somehow

--- src/hardware/pslab/protocol.c
+++ src/hardware/pslab/protocol.c
@@ -61,7 +61,6 @@ SR_PRIV int pslab_receive_data(int fd, int revents, void *cb_data)
 	for (i = 0; i < (int)devc->limits.limit_samples; i++) {
 		if(serial_read_blocking(serial, devc->short_int_buffer, 2 , serial_timeout(serial, 2)) < 2)
 			break;
-		sr_dbg("ln 59, raw value == %d , and voltage == %f ", *devc->short_int_buffer, scale(ch, *devc->short_int_buffer));
 		devc->data[i] = scale(ch, *devc->short_int_buffer);
 		samples_collected = i;
 	}
@@ -283,7 +282,6 @@ SR_PRIV int fetch_data(const struct sr_dev_inst *sdi)
 	*commands = RETRIEVE_BUFFER;
 	serial_write_blocking(serial,commands, 1, serial_timeout(serial, 1));
 	uint16_t startingposition =  ((struct channel_priv *)(ch->priv))->buffer_idx;
-	sr_dbg("ln 287 %hu", startingposition);
 	serial_write_blocking(serial,&startingposition, 2, serial_timeout(serial, 2));
 	uint16_t samples = devc->limits.limit_samples ;
 	serial_write_blocking(serial,&samples, 2, serial_timeout(serial,  2));
